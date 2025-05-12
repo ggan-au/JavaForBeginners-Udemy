fc3d539c44081abcf65a4b40834ba6cd407ab9e3
ai1002hash <glenganderton@proton.me>
2025-05-12 18:56:33 +1000

    Exercise 006 complete

---
+++ src/Set1/CodingExercise006.java
@@ -0,0 +1,18 @@
+package Set1;
+
+public class CodingExercise006 {
+    public static void main(String[] args) {
+        System.out.println(TimeConverter.convertDaysToMinutes(1));
+        System.out.println(TimeConverter.convertHoursToMinutes(7));
+    }
+}
+
+class TimeConverter {
+    public static int convertHoursToMinutes(int hours) {
+    return hours * 60;
+    }
+
+    public static int convertDaysToMinutes(int days) {
+    return days * convertHoursToMinutes(24);
+    }
+}
