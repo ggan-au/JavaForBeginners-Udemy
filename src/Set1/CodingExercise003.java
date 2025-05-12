b6a2937e912cba232afce116f68b6068cccea905
ai1002hash <glenganderton@proton.me>
2025-05-12 18:41:34 +1000

    Initial commit

---
+++ src/Set1/CodingExercise003.java
@@ -0,0 +1,11 @@
+package Set1;
+
+public class CodingExercise003 {
+    public static void main(String[] args) {
+        System.out.println(hello());
+    }
+
+    public static int hello() {
+        return 1;
+    }
+}
