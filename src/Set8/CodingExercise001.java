da076ef4ec1889e4bc0e05739856bdd33a5b0588
ai1002hash <glenganderton@proton.me>
2025-05-13 21:42:36 +1000

    Set 8 - Exercise 001 complete

---
+++ src/Set8/CodingExercise001.java
@@ -0,0 +1,31 @@
+package Set8;
+
+import java.util.Arrays;
+
+public class CodingExercise001 {
+    public static void main(String[] args) {
+        String str1 = "cinema";
+        String str2 = "iceman";
+        boolean actual = StringMagic.areAnagrams(str1, str2);
+        System.out.println(actual);
+    }
+}
+
+class StringMagic {
+    public static boolean areAnagrams(String str1, String str2) {
+        if (str1 == null || str2 == null) {
+            return false;
+        }
+
+        if (str1.length() != str2.length()) {
+            return false;
+        }
+
+        char[] str1Array = str1.toLowerCase().toCharArray();
+        char[] str2Array = str2.toLowerCase().toCharArray();
+        Arrays.sort(str1Array);
+        Arrays.sort(str2Array);
+
+        return Arrays.equals(str1Array, str2Array);
+    }
+}
